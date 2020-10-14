c0a3f55a766d622201bd18dc9fdd29e2d5109c81
unknown <johnatannkunz@gmail.com>
2020-10-14 16:29:57 -0300

    Segundo commit

---
+++ hello-word-html/src/main/java/br/edu/usj/ads/lpll/hellowordhtml/HelloWordHtmlApplication.java
@@ -0,0 +1,13 @@
+package br.edu.usj.ads.lpll.hellowordhtml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWordHtmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWordHtmlApplication.class, args);
+	}
+
+}
---
+++ hello-word-html/src/test/java/br/edu/usj/ads/lpll/hellowordhtml/HelloWordHtmlApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.usj.ads.lpll.hellowordhtml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWordHtmlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
